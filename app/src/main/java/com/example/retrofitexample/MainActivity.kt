685174c8b6d557ee4e41a99413f495494e4adafa
unknown <atakan.akin@metu.edu.tr>
2023-07-24 16:02:44 +0300

    final

--- app/src/main/java/com/example/retrofitexample/MainActivity.kt
+++ app/src/main/java/com/example/retrofitexample/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.retrofitexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+import com.example.retrofitexample.R
+import com.example.retrofitexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var quotex: String? = "No quote, Check your connection!"
+    var authorx: String? = "-Atakan"
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val quotesApi = RetrofitHelper.getInstance().create(QuotesApi::class.java)
+        binding.quote.setText(quotex)
+        binding.author.text = authorx
+        // launching a new coroutine
+        GlobalScope.launch {
+            val result = quotesApi.getQuotes()
+            if (result != null){
+                result.body().toString()
+                quotex = (result.body()?.results)?.get(0)?.content
+                authorx = (result.body()?.results)?.get(0)?.author
+                println(quotex)
+                println(authorx)
+                println("here")
+
+            }
+            // Checking the results
+            binding.quote.setText(quotex)
+            binding.author.text = authorx
+        }
+
+
+
+
+
+        setContentView(binding.root)
     }
 }
